af01168e5fbf88a846ee4f6f5adeafda307b47e2
frognar <sebastian.przyszlak@gmail.com>
2024-07-19 09:25:56 +0200

    Create Maybe with Some state

---
+++ src/dotMaybe/Maybe.cs
@@ -0,0 +1,23 @@
+﻿namespace DotMaybe;
+
+/// <summary>
+/// Represents an optional value of type T.
+/// </summary>
+/// <typeparam name="T">The type of the value that may or may not be present.</typeparam>
+/// /// <remarks>
+/// The Maybe monad is used to handle potentially missing values without using null references.
+/// It encapsulates the concept of an optional value, providing a safe way to represent and
+/// manipulate data that may or may not exist.
+/// </remarks>
+public readonly record struct Maybe<T>
+{
+    /// <summary>
+    /// Creates a Maybe instance containing the specified value.
+    /// </summary>
+    /// <param name="value">The value to be wrapped in a Maybe instance.</param>
+    /// <returns>A Maybe instance containing the specified value.</returns>
+    public static Maybe<T> Some(T value)
+    {
+        return default;
+    }
+}
---
+++ tests/dotMaybe.Tests.Unit/MaybeCreationTests.cs
@@ -0,0 +1,16 @@
+﻿using DotMaybe;
+using FluentAssertions;
+using FsCheck.Xunit;
+
+namespace dotMaybe.Tests.Unit;
+
+public class MaybeCreationTests
+{
+    [Property]
+    public void Maybe_WithValue_ShouldBeSome(int value)
+    {
+        Some.With(value)
+            .Should()
+            .Be(Some.With(value));
+    }
+}
