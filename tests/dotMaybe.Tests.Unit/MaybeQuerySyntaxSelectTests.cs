48f67b3495e3cce2385c787af474a4d678029afd
frognar <sebastian.przyszlak@gmail.com>
2024-07-21 10:32:22 +0200

    Add missing test for Select with None case

--- tests/dotMaybe.Tests.Unit/MaybeQuerySyntaxSelectTests.cs
+++ tests/dotMaybe.Tests.Unit/MaybeQuerySyntaxSelectTests.cs
@@ -10,4 +10,13 @@ public class MaybeQuerySyntaxSelectTests
             .Should()
             .Be(Some.With(value.ToString()));
     }
+
+    [Fact]
+    public void Select_WhenNone_ReturnsNone()
+    {
+        (from x in None.OfType<int>()
+                select x.ToString())
+            .Should()
+            .Be(None.OfType<string>());
+    }
 }
