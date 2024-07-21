0996cb4c74de1c3cc2fa243ce7b5885379d4b0b9
frognar <sebastian.przyszlak@gmail.com>
2024-07-21 09:38:09 +0200

    Add Map method for Maybe

--- src/dotMaybe/Maybe.cs
+++ src/dotMaybe/Maybe.cs
@@ -88,6 +88,24 @@ public readonly record struct Maybe<T>
     }
 
     /// <summary>
+    /// Transforms the value inside the Maybe if it exists.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result after applying the transformation.</typeparam>
+    /// <param name="map">The function to apply to the value if it exists.</param>
+    /// <returns>
+    /// A new Maybe instance containing the result of the transformation if the original Maybe had a value;
+    /// otherwise, returns an empty Maybe of the new type.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for transforming the value inside a Maybe without having to manually check
+    /// if the value exists.
+    /// </remarks>
+    public Maybe<TResult> Map<TResult>(Func<T, TResult> map)
+    {
+        return Match(Maybe<TResult>.None, v => Maybe<TResult>.Some(map(v)));
+    }
+
+    /// <summary>
     /// Creates a Maybe instance containing the specified value.
     /// </summary>
     /// <param name="value">The value to be wrapped in a Maybe instance.</param>
