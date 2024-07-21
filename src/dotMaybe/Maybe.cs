193396dc5938f7a0022ff16d8198f18cd4b941db
frognar <sebastian.przyszlak@gmail.com>
2024-07-21 10:18:53 +0200

    Split Maybe to multiple files

--- src/dotMaybe/Maybe.cs
+++ src/dotMaybe/Maybe.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace DotMaybe;
+﻿namespace DotMaybe;
 
 /// <summary>
 /// Represents an optional value of type T.
@@ -19,102 +16,6 @@ public readonly partial record struct Maybe<T>
     private Maybe(IMaybe maybe)
     {
         _maybe = maybe;
-    }
-
-    /// <summary>
-    /// Applies one of two functions based on whether the Maybe instance has a value or not.
-    /// </summary>
-    /// <typeparam name="TResult">The type of the result returned by both functions.</typeparam>
-    /// <param name="none">The function to execute if the Maybe instance is empty.</param>
-    /// <param name="some">The function to execute if the Maybe instance contains a value.</param>
-    /// <returns>
-    /// The result of executing either the 'none' function (if Maybe is empty) or
-    /// the 'some' function (if Maybe contains a value).
-    /// </returns>
-    /// <remarks>
-    /// This method provides a way to handle both cases (empty and non-empty) of a Maybe instance
-    /// in a single expression, similar to pattern matching.
-    /// </remarks>
-    public TResult Match<TResult>(Func<TResult> none, Func<T, TResult> some)
-    {
-        return _maybe switch
-        {
-            SomeType s => some(s.Value),
-            _ => none(),
-        };
-    }
-
-    /// <summary>
-    /// Asynchronously applies one of two functions based on whether the Maybe instance has a value or not.
-    /// </summary>
-    /// <typeparam name="TResult">The type of the result returned by both functions.</typeparam>
-    /// <param name="none">The asynchronous function to execute if the Maybe instance is empty.</param>
-    /// <param name="some">The asynchronous function to execute if the Maybe instance contains a value.</param>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains the result of executing
-    /// either the 'none' function (if Maybe is empty) or the 'some' function (if Maybe contains a value).
-    /// </returns>
-    public async Task<TResult> MatchAsync<TResult>(Func<Task<TResult>> none, Func<T, Task<TResult>> some)
-    {
-        return _maybe switch
-        {
-            SomeType s => await some(s.Value),
-            _ => await none(),
-        };
-    }
-
-    /// <summary>
-    /// Applies a synchronous function for the empty case and an asynchronous function for the non-empty case.
-    /// </summary>
-    /// <typeparam name="TResult">The type of the result returned by both functions.</typeparam>
-    /// <param name="none">The synchronous function to execute if the Maybe instance is empty.</param>
-    /// <param name="some">The asynchronous function to execute if the Maybe instance contains a value.</param>
-    /// <returns>
-    /// A task that represents the asynchronous operation. The task result contains the result of executing
-    /// either the 'none' function (if Maybe is empty) or the 'some' function (if Maybe contains a value).
-    /// </returns>
-    public async Task<TResult> MatchAsync<TResult>(Func<TResult> none, Func<T, Task<TResult>> some)
-    {
-        return _maybe switch
-        {
-            SomeType s => await some(s.Value),
-            _ => none(),
-        };
-    }
-
-    /// <summary>
-    /// Transforms the value inside the Maybe if it exists.
-    /// </summary>
-    /// <typeparam name="TResult">The type of the result after applying the transformation.</typeparam>
-    /// <param name="map">The function to apply to the value if it exists.</param>
-    /// <returns>
-    /// A new Maybe instance containing the result of the transformation if the original Maybe had a value;
-    /// otherwise, returns an empty Maybe of the new type.
-    /// </returns>
-    /// <remarks>
-    /// This method is useful for transforming the value inside a Maybe without having to manually check
-    /// if the value exists.
-    /// </remarks>
-    public Maybe<TResult> Map<TResult>(Func<T, TResult> map)
-    {
-        return Match(Maybe<TResult>.None, v => Maybe<TResult>.Some(map(v)));
-    }
-
-    /// <summary>
-    /// Applies a function that returns a Maybe to the value inside this Maybe if it exists.
-    /// </summary>
-    /// <typeparam name="TResult">The type of the value in the resulting Maybe.</typeparam>
-    /// <param name="bind">The function to apply to the value if it exists. This function should return a Maybe.</param>
-    /// <returns>
-    /// The result of applying the function if this Maybe has a value; otherwise, returns an empty Maybe.
-    /// </returns>
-    /// <remarks>
-    /// This method is useful for chaining operations that may or may not produce a value.
-    /// It helps in avoiding nested Maybe instances.
-    /// </remarks>
-    public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> bind)
-    {
-        return Match(Maybe<TResult>.None, bind);
     }
 
     /// <summary>
