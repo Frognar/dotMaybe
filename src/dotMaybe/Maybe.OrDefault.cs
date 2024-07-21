9fe7e7d1c454286a04a2d6e4a59c234aeed0f219
frognar <sebastian.przyszlak@gmail.com>
2024-07-21 15:32:19 +0200

    Add OrDefault method for Maybe

---
+++ src/dotMaybe/Maybe.OrDefault.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DotMaybe;
+
+public readonly partial record struct Maybe<T>
+{
+    /// <summary>
+    /// Returns the value if the Maybe instance contains one; otherwise, returns the specified default value.
+    /// </summary>
+    /// <param name="defaultValue">The default value to return if the Maybe instance is empty.</param>
+    /// <returns>
+    /// The value contained in the Maybe instance if it exists; otherwise, the specified default value.
+    /// </returns>
+    public T OrDefault(T defaultValue)
+    {
+        return _maybe switch
+        {
+            SomeType some => some.Value,
+            _ => defaultValue,
+        };
+    }
+}
---
+++ tests/dotMaybe.Tests.Unit/MaybeOrDefaultTests.cs
@@ -0,0 +1,22 @@
+﻿namespace dotMaybe.Tests.Unit;
+
+public class MaybeOrDefaultTests
+{
+    [Property]
+    public void OrDefault_WhenSome_ReturnsInternalValue(int value)
+    {
+        Some.With(value)
+            .OrDefault(-1)
+            .Should()
+            .Be(value);
+    }
+
+    [Property]
+    public void OrDefault_WhenNone_ReturnsDefault(int value)
+    {
+        None.OfType<int>()
+            .OrDefault(value)
+            .Should()
+            .Be(value);
+    }
+}
