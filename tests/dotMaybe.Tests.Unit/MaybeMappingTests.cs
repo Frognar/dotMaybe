f2af89ea0b4cd8421496b9d6d8118d2fd8afdf5c
frognar <sebastian.przyszlak@gmail.com>
2024-07-22 20:47:26 +0200

    Add Maybe.Map3 function

--- tests/dotMaybe.Tests.Unit/MaybeMappingTests.cs
+++ tests/dotMaybe.Tests.Unit/MaybeMappingTests.cs
@@ -23,15 +23,21 @@ public class MaybeMappingTests
     [Property]
     public void Map2_WhenBothSome_ReturnsSome(int value, decimal otherValue)
     {
-        Maybe.Map2(Some.With(value), Some.With(otherValue), (v1, v2) => $"{v1} : {v2}")
+        Maybe.Map2(
+                Some.With(value),
+                Some.With(otherValue),
+                (v1, v2) => $"{v1} : {v2}")
             .Should()
             .Be(Some.With($"{value} : {otherValue}"));
     }
 
     [Property]
-    public void Map2_WhenFirstIsNone_ReturnsNone(decimal value)
+    public void Map2_WhenFirstIsNone_ReturnsNone(decimal otherValue)
     {
-        Maybe.Map2(None.OfType<int>(), Some.With(value), (v1, v2) => $"{v1} : {v2}")
+        Maybe.Map2(
+                None.OfType<int>(),
+                Some.With(otherValue),
+                (v1, v2) => $"{v1} : {v2}")
             .Should()
             .Be(None.OfType<string>());
     }
@@ -39,7 +45,58 @@ public class MaybeMappingTests
     [Property]
     public void Map2_WhenSecondIsNone_ReturnsNone(int value)
     {
-        Maybe.Map2(Some.With(value), None.OfType<decimal>(), (v1, v2) => $"{v1} : {v2}")
+        Maybe.Map2(
+                Some.With(value),
+                None.OfType<decimal>(),
+                (v1, v2) => $"{v1} : {v2}")
+            .Should()
+            .Be(None.OfType<string>());
+    }
+
+    [Property]
+    public void Map3_WhenAllSome_ReturnsSome(int value, decimal otherValue, char yetAnotherValue)
+    {
+        Maybe.Map3(
+                Some.With(value),
+                Some.With(otherValue),
+                Some.With(yetAnotherValue),
+                (v1, v2, v3) => $"{v1} : {v2} : {v3}")
+            .Should()
+            .Be(Some.With($"{value} : {otherValue} : {yetAnotherValue}"));
+    }
+
+    [Property]
+    public void Map3_WhenFirstIsNone_ReturnsNone(decimal otherValue, char yetAnotherValue)
+    {
+        Maybe.Map3(
+                None.OfType<int>(),
+                Some.With(otherValue),
+                Some.With(yetAnotherValue),
+                (v1, v2, v3) => $"{v1} : {v2} : {v3}")
+            .Should()
+            .Be(None.OfType<string>());
+    }
+
+    [Property]
+    public void Map3_WhenSecondIsNone_ReturnsNone(int value, char yetAnotherValue)
+    {
+        Maybe.Map3(
+                Some.With(value),
+                None.OfType<decimal>(),
+                Some.With(yetAnotherValue),
+                (v1, v2, v3) => $"{v1} : {v2} : {v3}")
+            .Should()
+            .Be(None.OfType<string>());
+    }
+
+    [Property]
+    public void Map3_WhenThirdIsNone_ReturnsNone(int value, decimal otherValue)
+    {
+        Maybe.Map3(
+                Some.With(value),
+                Some.With(otherValue),
+                None.OfType<char>(),
+                (v1, v2, v3) => $"{v1} : {v2} : {v3}")
             .Should()
             .Be(None.OfType<string>());
     }
