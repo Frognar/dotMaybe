fb3cbb08edb653a5195a5ef180841bba2e746bf9
frognar <sebastian.przyszlak@gmail.com>
2024-07-26 16:21:37 +0200

    Add Fold method for Maybe

---
+++ src/dotMaybe/Maybe.Fold.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DotMaybe;
+
+public readonly partial record struct Maybe<T>
+{
+    /// <summary>
+    /// Applies an aggregation function to the value contained in Maybe, if it exists.
+    /// </summary>
+    /// <typeparam name="TState">The type of the state and aggregation result.</typeparam>
+    /// <param name="state">The initial state of the aggregation.</param>
+    /// <param name="folder">The aggregation function that takes the current state and value, returning a new state.</param>
+    /// <returns>
+    /// If Maybe contains a value (Some), returns the result of applying the folder function to that value and the initial state.
+    /// If Maybe does not contain a value (None), returns the initial state without modification.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for safely processing potentially non-existent values
+    /// without the need for manual existence checks.
+    /// </remarks>
+    public TState Fold<TState>(TState state, Func<TState, T, TState> folder)
+    {
+        return Match(() => state, value => folder(state, value));
+    }
+}
---
+++ tests/dotMaybe.Tests.Unit/MaybeFoldingTests.cs
@@ -0,0 +1,22 @@
+﻿namespace dotMaybe.Tests.Unit;
+
+public class MaybeFoldingTests
+{
+    [Property]
+    public void Fold_WhenSome_ReturnsStateUpdatedByGivenFunction(int state, int value)
+    {
+        Some.With(value)
+            .Fold(state, (s, v) => s + v)
+            .Should()
+            .Be(state + value);
+    }
+
+    [Property]
+    public void Fold_WhenNone_ReturnsStateUnchanged(int state)
+    {
+        None.OfType<int>()
+            .Fold(state, (s, v) => s + v)
+            .Should()
+            .Be(state);
+    }
+}
