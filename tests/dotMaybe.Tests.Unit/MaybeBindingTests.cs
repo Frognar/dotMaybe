27766e0eb12581b8fd707e0569563f4f4bbaff31
frognar <sebastian.przyszlak@gmail.com>
2024-07-28 12:03:35 +0200

    Add BindAsync for Maybe

--- tests/dotMaybe.Tests.Unit/MaybeBindingTests.cs
+++ tests/dotMaybe.Tests.Unit/MaybeBindingTests.cs
@@ -19,4 +19,22 @@ public class MaybeBindingTests
             .Should()
             .Be(None.OfType<string>());
     }
+
+    [Property]
+    public async Task BindAsync_WhenSome_TransformsValueAsync(int value)
+    {
+        (await Some.With(value)
+                .BindAsync(async v => await Task.FromResult(Some.With(v.ToString()))))
+            .Should()
+            .Be(Some.With(value.ToString()));
+    }
+
+    [Fact]
+    public async Task BindAsync_WhenNone_ReturnsNone()
+    {
+        (await None.OfType<int>()
+                .BindAsync(async v => await Task.FromResult(Some.With(v.ToString()))))
+            .Should()
+            .Be(None.OfType<string>());
+    }
 }
